cef4f4bd2ac994f52859f099707c7781cc71bc9d
Оразымбетова Шолпан <@step.kz>
2023-11-28 21:15:57 +0600

    lesson_07 files

--- samplesite/settings.py
+++ samplesite/settings.py
@@ -125,3 +125,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ABSOLUTE_URL_OVERRIDES = {
+#     # 'bboard.rubric': lambda rec: "/%s" % rec.pk,
+#     'bboard.rubric': lambda rec: f"/{rec.pk}/",
+# }
