0fc895fef3b86b2a5fc73c853835960fe612f725
Оразымбетова Шолпан <@step.kz>
2023-12-05 21:37:19 +0600

    additional lesson_09

--- testapp/models.py
+++ testapp/models.py
@@ -6,6 +6,9 @@ class AdvUser(models.Model):
     is_activated = models.BooleanField(default=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
+
 
 class Spare(models.Model):
     name = models.CharField(max_length=30)
