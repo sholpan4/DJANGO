f25cf4d5fb763d002c8b759992c78b98d5140e6f
Оразымбетова Шолпан <@step.kz>
2024-01-30 20:11:52 +0600

    lesson_22 files

--- bboard/forms.py
+++ bboard/forms.py
@@ -1,15 +1,72 @@
-from django.forms import ModelForm
+# from django.forms import ModelForm, modelform_factory, DecimalField
+from django import forms
+from django.contrib.auth.models import User
+from django.forms.widgets import Select
 from .models import Bb, Rubric
 
 
-class BbForm(ModelForm):
+class BbForm(forms.ModelForm):
+    price = forms.DecimalField(label='Цена', decimal_places=2)
+    rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(),
+                                    label='Рубрика',
+                                    help_text='Не забудь выбрать рубрику!',
+                                    widget=forms.widgets.Select(attrs={'size': 8}))
+    datetime = forms.DateField(widget=forms.widgets.SelectDateWidget())
+
     class Meta:
         model = Bb
         fields = ('title', 'content', 'price', 'rubric')
-        # __all__
+        labels = {'title': 'Название товара'}
 
 
-class RubricForm(ModelForm):
+class RegisterUserForm(forms.ModelForm):
+    password1 = forms.CharField(label='Парол')
+    password2 = forms.CharField(label='Пароль (повторно)')
+
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
+
+
+# полное об #1
+# class BbForm(forms.ModelForm):
+#     title = forms.CharField(label='Название товара')
+#     content = forms.CharField(label='Описание',
+#                               widget=forms.widgets.Textarea())
+#     price = forms.DecimalField(label='Цена', decimal_places=2)
+#     rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(),
+#                                     label='Рубрика',
+#                                     help_text='Не забудь выбрать рубрику!',
+#                                     widget=forms.widgets.Select(attrs={'size': 8}))
+#
+#     class Meta:
+#         model = Bb
+#         fields = ('title', 'content', 'price', 'rubric')
+
+# Фабрика классов
+# BbForm = modelform_factory(
+#     Bb,
+#     fields=('title', 'content', 'price', 'rubric'),
+#     labels={'title': 'Название товара'},
+#     help_texts={'rubric': 'Не забудь выбрать рубрику!'},
+#     field_classes={'price': DecimalField},
+#     widgets={'rubric': Select(attrs={'size': 8})},
+# )
+
+# Быстрые обявления
+# class BbForm(ModelForm):
+#     class Meta:
+#         model = Bb
+#         fields = ('title', 'content', 'price', 'rubric')
+#         labels = {'title': 'Название товара'}
+#         help_texts = {'rubric': 'Не забудь выбрать рубрику!'}
+#         field_classes = {'price': DecimalField}
+#         widgets = {'rubric': Select(attrs={'size': 8})}
+# __all__
+
+
+class RubricForm(forms.ModelForm):
     class Meta:
         model = Rubric
         fields = ('name', )
