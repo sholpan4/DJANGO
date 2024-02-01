389e275f2ec83d12211b9113273f1f50cb3bd1e9
Оразымбетова Шолпан <@step.kz>
2024-02-01 20:52:00 +0600

    lesson_24 files

--- bboard/forms.py
+++ bboard/forms.py
@@ -1,18 +1,42 @@
 # from django.forms import ModelForm, modelform_factory, DecimalField
 from django import forms
 from django.contrib.auth.models import User
+from django.core import validators
+from django.core.exceptions import ValidationError
 from django.forms.widgets import Select
 from .models import Bb, Rubric
 
 
 class BbForm(forms.ModelForm):
+    title = forms.CharField(label='название товара',
+                            validators=[validators.RegexValidator(regex='^.{4,}$')],
+                            error_messages={'invalid': 'Слишком короткое название товара!'}
+                            )
+
     price = forms.DecimalField(label='Цена', decimal_places=2)
     rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(),
                                     label='Рубрика',
                                     help_text='Не забудь выбрать рубрику!',
                                     widget=forms.widgets.Select(attrs={'size': 8}))
     datetime = forms.DateField(widget=forms.widgets.SelectDateWidget())
-    # published = forms.DateField(widget=forms.widgets.SelectDateWidget(empty_label=('Выберите год', 'Выберите месяц', 'Выберите дату')))
+
+    def clean_title(self):
+        # val = self.cleaned_data['title']
+        val = self.cleaned_data.get('title')
+        if val == 'Прошлогдний снег':
+            raise ValidationError('К продаже не допускается!')
+        return val
+
+    def clean(self):
+        super().clean()
+        errors = {}
+
+        if not self.cleaned_data.get('content'):
+            errors['content'] = ValidationError('Укажите описание продаваемого товара')
+        if self.cleaned_data.get('price') < 0:
+            errors['price'] = ValidationError('Укажите неотрицательное значение')
+        if errors:
+            raise ValidationError(errors)
 
     class Meta:
         model = Bb
