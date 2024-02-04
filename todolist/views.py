2f7868ecd0ba0dd105f13c7834f4cb48578182cf
sholpan4 <b.o.sholpan44@gmail.com>
2024-02-05 00:51:06 +0600

    lesson22_hw24 files

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
@@ -5,7 +6,24 @@ from .forms import TaskForm
 
 def task_list(request):
     tasks = Task.objects.all()
-    return render(request, 'task_list.html', {'tasks': tasks})
+    paginator = Paginator(tasks, 2, orphans=2)
+
+    page_num = request.GET.get('page', 1)
+    try:
+        page_num = int(page_num)
+    except ValueError:
+        page_num = 1
+
+    # if 'page' in request.GET:
+    #     page_num = request.GET['page']
+    # else:
+    #     page_num = 1
+
+    page = paginator.get_page(page_num)
+
+    context = {'page_obj': page, 'tasks': page.object_list}
+
+    return render(request, 'task_list.html', context)
 
 def create_task(request):
     if request.method == 'POST':
