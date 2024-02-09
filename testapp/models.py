9ac1e879bd1c9af51d42ce92b625c04e21d9f8a0
Оразымбетова Шолпан <@step.kz>
2024-02-09 21:14:38 +0600

    lesson_27 files

--- testapp/models.py
+++ testapp/models.py
@@ -19,7 +19,14 @@ class Spare(models.Model):
 
 class Machine(models.Model):
     name = models.CharField(max_length=30)
-    spares = models.ManyToManyField(Spare)
+    spares = models.ManyToManyField(Spare, through='Kit', through_fields=('machine', 'spare'))
+    # spares = models.ManyToManyField(Spare)
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Kit(models.Model):
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    spare = models.ForeignKey(Spare, on_delete=models.CASCADE)
+    count = models.PositiveIntegerField(default=0)
