ac3ed8ff9939b61c42ff199a885027db3a9361c4
Оразымбетова Шолпан <@step.kz>
2024-02-13 21:28:28 +0600

    lesson_28_hw files

--- testapp/models.py
+++ testapp/models.py
@@ -27,11 +27,29 @@ class Machine(models.Model):
     def __str__(self):
         return f'{self.name}'
 
+    # class Meta:
+    #     ordering = ['name']
+
 
 class Kit(models.Model):
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
     spare = models.ForeignKey(Spare, on_delete=models.CASCADE)
     count = models.PositiveIntegerField(default=0)
+
+
+# class AditMachine(Machine):
+#     car_model = models.CharField(max_length=30)
+#     car_year = models.PositiveIntegerField()
+#     car_make = models.CharField(max_length=30)
+#
+#     class Meta(Machine.Meta):
+#         abstract = True
+#
+#
+# # class OrdMachine(AditMachine):
+# #     class Meta(AditMachine.Meta):
+# #         proxy = True
+# #         ordering = ['car_year', 'car_make']
 
 
 class Note(models.Model):
@@ -67,3 +85,25 @@ class PrivateMessage(Message):
 #
 #     class Meta(Message.Meta):
 #         pass
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    class Meta:
+        abstract = True
+        ordering = ['name']
+
+
+class Children(Person):
+    age = models.PositiveIntegerField()
+
+    class Meta(Person.Meta):
+        pass
+
+
+class Grandchildren(Children):
+    class Meta(Children.Meta):
+        proxy = True
+        ordering = ['age']
